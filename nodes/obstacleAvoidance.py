9e4f3d22711f1a93289d43a75df7a9d880170ee6
aribo <aribo@aribo.com>
2015-02-08 12:08:42 -0500

    A lot of stuff

--- nodes/obstacleAvoidance.py
+++ nodes/obstacleAvoidance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-PACKAGE = 'chris_pioneer_control'
+PACKAGE = 'aribo'
 import roslib
 roslib.load_manifest(PACKAGE)
 import rospy
@@ -15,25 +15,38 @@ from math import sin, cos, pi, sqrt, asin, atan2, log
 class obstacleAvoidance:
 	def __init__(self):
 		self.gain = rospy.get_param('lidar/gain') # defines the gain for the logarithmic function that will define how quickly an object must be avoided
-		self.twist_pub = rospy.Publisher('/OA/Twist',Twist)
+		self.twist_pub = rospy.Publisher('/VC/obstacle',Twist)
+		rospy.Subscriber('scan',  LaserScan , self.lidarFeedback)
 
 	def lidarFeedback(self,msg):
-		ouput = Twist()
+                self.outTwist = Twist()
+                self.outTwist.linear.x = 0.0
+                self.outTwist.linear.y = 0.0
+                self.outTwist.linear.z = 0.0
+                self.outTwist.angular.x = 0.0 # roll
+                self.outTwist.angular.y = 0.0 # pitch
+                self.outTwist.angular.z = 0.0 # yaw
+
 		sumForces = [0,0]
+		count = 0
 		for i in range(0,len(msg.ranges)):
 			# 0 rads is front
 			curAngle = ((i*msg.angle_increment)+msg.angle_min)
 			# only for objects within a meter
 			# x and y components
-			magnitude = -1/msg.ranges[i]
-			curForce = [cos(curAngle)*magnitude,sin(curAngle)*magnitude]
-			sumForces[0] += curForce[0]
-			sumForces[1] += curForce[1]
-		# magnitude times direction yields the final result
-		output.angular.z = sqrt(sumForces[1]*sumForces[1]+sumForces[0]*sumForces[0])*atan2(sumForces[1],sumForces[0])
+			if msg.ranges[i]<0.75 and curAngle > -pi/2 and curAngle < pi/2:
+				count+=1
+				sumForces[0] += sin(curAngle)*1/(msg.ranges[i]*msg.ranges[i])
+				sumForces[1] += -1*cos(curAngle)*1/(msg.ranges[i]*msg.ranges[i])
+		if not count == 0:
+			sumForces[0] = sumForces[0] / count
+			sumForces[1] = sumForces[1] / count
+		print sumForces
+		self.outTwist.linear.x = sumForces[1]
+		self.outTwist.angular.z = sumForces[0]
+		self.twist_pub.publish(self.outTwist)
 		
 if __name__ == '__main__':
 	rospy.init_node('obstacle_avoidance')
 	controller = obstacleAvoidance()
-	rospy.Subscriber('/OA/Lidar',  LaserScan , controller.lidarFeedback)
 	rospy.spin()
--- nodes/velocityControl.py
+++ nodes/velocityControl.py
@@ -6,13 +6,15 @@ roslib.load_manifest(PACKAGE)
 import rospy
 
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 from math import sin, cos, pi, sqrt, asin, atan2, log
 from threading import Thread
+import time
 
 # actually controls the velocity based on the multiple inputs provided
 class velocityControl:
 	def __init__(self):
-		self.sleep_time = 1 / rospy.get_param('pioneer/updateFreq')
+		self.update_freq = rospy.get_param('pioneer/updateFreq')
 		self.teleopGain = rospy.get_param('pioneer/teleopGain')
 		self.obstacleGain = rospy.get_param('pioneer/obstacleGain')
 		self.out_twist_pub = rospy.Publisher('/VC/out', Twist)
@@ -25,28 +27,40 @@ class velocityControl:
 		self.outTwist.angular.z = 0.0 # yaw
 		rospy.init_node('velocity_control', anonymous=True)
 		self.teleopData = None
+		self.teleopTime = None
 		rospy.Subscriber('/VC/teleop', Twist, self.teleop)
 		self.obstacleData = None
 		rospy.Subscriber('/VC/obstacle', Twist, self.obstacle)
+		self.state = "teleop"
+		rospy.Subscriber('VC/state', String, self.state) 
 		self.alive=True
 
 	def teleop(self, data):
+		self.teleopTime = time.time()
 		self.teleopData = data
+
+	def state(self, data):
+		self.state = data.data
 
 	def obstacle(self,data):
 		self.obstacleData = data
 
 	def updateCommandVelocity(self):
+		rate = rospy.Rate(self.update_freq)
 		while self.alive == True and not rospy.is_shutdown():
-			self.outTwist.linear.x = 0.0
-			self.outTwist.angular.z = 0.0
-			if self.teleopData:
-				self.outTwist.linear.x += self.teleopData.linear.x*self.teleopGain
-				self.outTwist.angular.z += self.teleopData.angular.z*self.teleopGain
-			if self.obstacleData:
-				self.outTwist.linear.x += self.obstacleData.linear.x*self.obstacleGain
-				self.outTwist.angular.z += self.obstacleData.angular.z*self.obstacleGain
+		   self.outTwist.linear.x = 0.0
+		   self.outTwist.angular.z = 0.0
+		   if self.obstacleData and self.teleopData:
+			self.outTwist.linear.x += self.obstacleData.linear.x*self.obstacleGain
+			self.outTwist.angular.z += self.obstacleData.angular.z*self.obstacleGain
+			if self.state=="teleop": # and abs(self.obstacleData.linear.x)<2.0 and abs(self.obstacleData.angular.z)<2.0:
+			    if (time.time()-self.teleopTime)<1.0:
+				self.outTwist.linear.x += (self.teleopData.linear.x*self.teleopGain)
+				self.outTwist.angular.z += (self.teleopData.angular.z*self.teleopGain)
+			self.outTwist.linear.x = -self.outTwist.linear.x
+			self.outTwist.angular.z = -self.outTwist.angular.z
 			self.out_twist_pub.publish(self.outTwist)
+			rate.sleep()
 			
 		time.sleep(self.sleep_time)
 if __name__ == '__main__':
