1dfcdd7a862019160d32e7c83aab8886becae412
Kenneth Chaney <kpc49@drexel.edu>
2015-09-11 15:35:29 -0400

    cleaned up pieces of the aribo launch strucutre and webpage

--- nodes/velocityControl.py
+++ nodes/velocityControl.py
@@ -13,51 +13,51 @@ import time
 
 # actually controls the velocity based on the multiple inputs provided
 class velocityControl:
-	def __init__(self):
-		self.update_freq = rospy.get_param('pioneer/updateFreq')
-		self.teleopGain = rospy.get_param('pioneer/teleopGain')
-		self.out_twist_pub = rospy.Publisher('/VC/out', Twist)
-		self.outTwist = Twist()
-		self.outTwist.linear.x = 0.0
-		self.outTwist.linear.y = 0.0
-		self.outTwist.linear.z = 0.0
-		self.outTwist.angular.x = 0.0 # roll
-		self.outTwist.angular.y = 0.0 # pitch
-		self.outTwist.angular.z = 0.0 # yaw
-		rospy.init_node('velocity_control', anonymous=True)
-		self.teleopData = None
-		self.teleopTime = None
-		rospy.Subscriber('/VC/teleop', Twist, self.teleop)
-		self.alive=True
-
-	def teleop(self, data):
-		self.teleopTime = time.time()
-		self.teleopData = data
-
-	def stateCb(self, data):
-		self.state = data.data
-		print self.state
-
-	def obstacle(self,data):
-		self.obstacleData = data
-
-	def navCb(self, data):
-		self.navData = data
-
-	def updateCommandVelocity(self):
-		rate = rospy.Rate(self.update_freq)
-		while self.alive == True and not rospy.is_shutdown():
-		   self.outTwist.linear.x = 0.0
-		   self.outTwist.angular.z = 0.0
-		   if self.teleopTime and self.teleopData:
-			   if (time.time()-self.teleopTime)<1.0:
-				   self.outTwist.linear.x += (self.teleopData.linear.x*self.teleopGain)
-				   self.outTwist.angular.z += (self.teleopData.angular.z*self.teleopGain)
-			print self.outTwist
-			self.out_twist_pub.publish(self.outTwist)
-			rate.sleep()
-			
-		time.sleep(self.sleep_time)
+  def __init__(self):
+    self.update_freq = rospy.get_param('pioneer/updateFreq')
+    self.sleep_time = 1/self.update_freq
+    self.teleopGain = rospy.get_param('pioneer/teleopGain')
+    self.out_twist_pub = rospy.Publisher('/VC/out', Twist)
+    self.outTwist = Twist()
+    self.outTwist.linear.x = 0.0
+    self.outTwist.linear.y = 0.0
+    self.outTwist.linear.z = 0.0
+    self.outTwist.angular.x = 0.0 # roll
+    self.outTwist.angular.y = 0.0 # pitch
+    self.outTwist.angular.z = 0.0 # yaw
+    rospy.init_node('velocity_control', anonymous=True)
+    self.teleopData = None
+    self.teleopTime = None
+    rospy.Subscriber('/VC/teleop', Twist, self.teleop)
+    self.alive=True
+  
+  def teleop(self, data):
+  	self.teleopTime = time.time()
+  	self.teleopData = data
+  
+  def stateCb(self, data):
+  	self.state = data.data
+  	print self.state
+  
+  def obstacle(self,data):
+  	self.obstacleData = data
+  
+  def navCb(self, data):
+  	self.navData = data
+  
+  def updateCommandVelocity(self):
+    rate = rospy.Rate(self.update_freq)
+    while self.alive == True and not rospy.is_shutdown():
+      self.outTwist.linear.x = 0.0
+      self.outTwist.angular.z = 0.0
+      if self.teleopTime and self.teleopData:
+         if (time.time()-self.teleopTime)<1.0:
+      	   self.outTwist.linear.x += (self.teleopData.linear.x*self.teleopGain)
+      	   self.outTwist.angular.z += (self.teleopData.angular.z*self.teleopGain)
+      print self.outTwist
+      self.out_twist_pub.publish(self.outTwist)
+      rate.sleep()
+      time.sleep(self.sleep_time)
 
 if __name__ == '__main__':
 	try:
