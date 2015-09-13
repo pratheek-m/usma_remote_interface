f7a60f1458c591b97252ba256f6fd809301e5215
Kenneth Chaney <kpc49@drexel.edu>
2015-09-13 17:17:11 -0400

    setup script added for apache2

---
+++ scripts/usma_remote_interface_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -i
+
+# sets up a couple extra pieces to let the OS know where openjaus is located
+
+source $HOME/.bashrc
+
+rospd usma_remote_interface/webpage
+
+sudo apt-get update
+sudo apt-get install apache2
+
+
+echo "
+<VirtualHost *:80>
+	ServerAdmin webmaster@localhost
+	DocumentRoot $(pwd)
+  <Directory $(pwd)>
+    Require all granted
+  </Directory>
+
+	ErrorLog"' ${APACHE_LOG_DIR}'"/error.log
+	CustomLog"' ${APACHE_LOG_DIR}'"/access.log combined
+
+</VirtualHost>
+" >> tmp.conf
+
+sudo rm /etc/apache2/sites-enabled/*
+sudo mv tmp.conf /etc/apache2/sites-available/usma_remote_interface.conf
+sudo ln -s /etc/apache2/sites-available/usma_remote_interface.conf /etc/apache2/sites-enabled/usma_remote_interface.conf
+
+sudo service apache2 restart
+
+
+rospd
+
+
