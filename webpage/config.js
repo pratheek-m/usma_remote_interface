fda71ebd6c9697b6bf527ecfcb5bb8cc6675125a
Kenneth Chaney <kpc49@drexel.edu>
2016-04-13 00:53:08 -0400

    added functionality for sensor states that include safe, warning, and danger. Levels are configured in the config.js file

--- webpage/config.js
+++ webpage/config.js
@@ -1,4 +1,6 @@
-var ip='192.168.200.50'
+var ip='129.25.56.198'
 var cmdTopic='/usma_remote/webcmd'
 var imageTopic='/usb_cam/image_raw'
 var sensorList=['TEMP','CO2','CO','METHANE','GAS']
+var warningMin=[10,10,10,10,10]
+var dangerMin=[30,30,30,30,30]
